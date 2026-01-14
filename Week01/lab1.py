254267c5de6fc65e9aaec2aea0578e73f378a27d
joseanemsilva <joseanemaria.dasilva@georgebrown.ca>
2026-01-14 13:06:54 -0500

    Add lab exercises for Week 01

---
+++ Week01/lab1.py
@@ -0,0 +1,25 @@
+# Sample Coding Question 01 - Week 01
+# Joseane Maria da Silva
+
+
+# Defining variables: Array list
+numbers = [1, 4, 7, 9]
+
+# Order of operations
+a = 1
+b = 2
+c = 3
+d = 4
+
+e = a - ((b ** c) // d) + (a % c)
+print("The result is:", e)
+
+# Formatting: format function
+temperature = 32.6
+print("The temperature is: {:.3f} degrees Celsius." .format(temperature))
+
+# Common functions: User input
+userAge = int(input("Enter your age:"))
+userAge = userAge + 22
+
+print("Now showing the shop items filtered by age:", userAge)
